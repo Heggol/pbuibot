62ebe3d913118910cf7aeaec554ad2680f84ff77
Heggo <maxiberkes@gmail.com>
2025-03-01 14:22:28 +0100

    Fixed the state reset to always create a new message

--- src/events/socket/stateReset.js
+++ src/events/socket/stateReset.js
@@ -4,11 +4,10 @@ const { sendToMatchChannel } = require('../../services/discordService');
 
 module.exports = function () {
     console.log('State reset');
+    config.state.lastMessage = null;
     if (config.state.isActive && config.state.matchChannel) {
         sendToMatchChannel('⚠️ Pick/Ban state has been reset');
     }
 
     config.state.lastState = null;
-
-    config.state.tiebreakerMapId = null;
 };
